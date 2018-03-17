840a72580712fed85a21e495b811bdc68a11d1fc
breal5000 <34284810+breal5000@users.noreply.github.com>
2018-03-16 19:35:02 -0700

    some html and css

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// mantra during coding
+alert("I ain't scared of you muthafucka");
+// ask player to initialize game
+// create a variables to generate random hidden number
+variable = randomNum (Math.floor((Math.random() * 120) + 1))
+// create variables to generate random crystal number assignments
+// run loops to add numbers to win or lose.
+// create ending function to declare winner or looser
+// restart game 
