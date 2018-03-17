9274657e438385c5ad2f4dac22e7ba4df131e30c
breal5000 <34284810+breal5000@users.noreply.github.com>
2018-03-17 14:08:04 -0700

    some changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,16 @@
 // mantra during coding
 alert("I ain't scared of you muthafucka");
 // ask player to initialize game
+console.log("its working");
+        
+document.onkeyup = function (event) {
+    console.log("the event is", event.key);
+    console.log("the key is", event.key)
+    var guessedNumberDiv = document.getElementById("guesses_numbers");
+    var guessedNumbersDiv = document.getElementById("guessed_numbers");
+}
 // create a variables to generate random hidden number
-variable = randomNum (Math.floor((Math.random() * 120) + 1))
+variable = randomNumber (Math.floor((Math.random() * 120) + 1))
 // create variables to generate random crystal number assignments
 // run loops to add numbers to win or lose.
 // create ending function to declare winner or looser
