7159d51982c7865b821556b518037581e0554d4b
breal5000 <34284810+breal5000@users.noreply.github.com>
2018-03-19 10:25:57 -0700

    random added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,12 +9,14 @@ var win;
 //var resetAndStart = function () {
     //$(".crystals").empty();
    //console.log("its working");
-    
+randomNumber = Math.floor(Math.random() * 120) + 30; // from 30 and up
+    console.log(randomNumber);
+$("#result").html("Random Number: ");   
 for(var i = 0; i < 4; i++){
- 
-    var crystal = $("<div>");
- 
+var random = Math.floor(Math.random() * 12);
+    
+var crystal = $("<div>");
+        crystal.attr({"class": 'crystal', "data-random": random});
     $(".crystals").append(crystal);
     
-    console.log("its working");
 }   
